f1017c6f6c9a7c21f6a6eacba5887d1073d87512
zaurela <zaureladibra@gmail.com>
2021-07-19 16:15:17 +0200

    first commit

---
+++ Hungman/Hungman_WebForm.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Hungman_WebForm.aspx.cs" Inherits="Hungman_Game__HW2.Hungman_WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script language="javascript" type="text/javascript">
+
+</script>
+    <link rel="Stylesheet" href="Hungman_Style.css" />
+</head>&nbsp;<body style="height: 335px"><form id="form1" runat="server">
+  <p><b> <asp:Label ID="label6" runat="server" CssClass="word" Text="HUNGMAN GAME"></asp:Label></b></p>
+  <p>
+       
+    <asp:Button CssClass="btn" ID="A" runat="server" onclick="LetterGuessed" Text="A" />
+    <asp:Button CssClass="btn" ID="B" runat="server" onclick="LetterGuessed" Text="B" />
+    <asp:Button CssClass="btn" ID="C" runat="server" onclick="LetterGuessed" Text="C" />
+    <asp:Button CssClass="btn" ID="D" runat="server" onclick="LetterGuessed" Text="D" />
+    <asp:Button CssClass="btn" ID="E" runat="server" onclick="LetterGuessed" Text="E" />
+    <asp:Button CssClass="btn" ID="F" runat="server" onclick="LetterGuessed" Text="F" />
+    <asp:Button CssClass="btn" ID="G" runat="server" onclick="LetterGuessed" Text="G" />
+    <asp:Button CssClass="btn" ID="H" runat="server" onclick="LetterGuessed" Text="H" />
+    <asp:Button CssClass="btn" ID="I" runat="server" onclick="LetterGuessed" Text="I" />
+    <asp:Button CssClass="btn" ID="J" runat="server" onclick="LetterGuessed" Text="J" />
+    <asp:Button CssClass="btn" ID="K" runat="server" onclick="LetterGuessed" Text="K" />
+    <asp:Button CssClass="btn" ID="L" runat="server" onclick="LetterGuessed" Text="L" />
+    <asp:Button CssClass="btn" ID="M" runat="server" onclick="LetterGuessed" Text="M" />
+    <asp:Button CssClass="btn" ID="N" runat="server" onclick="LetterGuessed" Text="N" />
+    <asp:Button CssClass="btn" ID="O" runat="server" onclick="LetterGuessed" Text="O" />
+    <asp:Button CssClass="btn" ID="P" runat="server" onclick="LetterGuessed" Text="P" />
+    <asp:Button CssClass="btn" ID="Q" runat="server" onclick="LetterGuessed" Text="Q" />
+    <asp:Button CssClass="btn" ID="R" runat="server" onclick="LetterGuessed" Text="R" />
+    <asp:Button CssClass="btn" ID="S" runat="server" onclick="LetterGuessed" Text="S" />
+    <asp:Button CssClass="btn" ID="T" runat="server" onclick="LetterGuessed" Text="T" />
+    <asp:Button CssClass="btn" ID="U" runat="server" onclick="LetterGuessed" Text="U" />
+    <asp:Button CssClass="btn" ID="V" runat="server" onclick="LetterGuessed" Text="V" />
+    <asp:Button CssClass="btn" ID="W" runat="server" onclick="LetterGuessed" Text="W" />
+    <asp:Button CssClass="btn" ID="X" runat="server" onclick="LetterGuessed" Text="X" />
+    <asp:Button CssClass="btn" ID="Y" runat="server" onclick="LetterGuessed" Text="Y" />
+    <asp:Button CssClass="btn" ID="Z" runat="server" onclick="LetterGuessed" Text="Z" />
+  </p>
+
+    <asp:Button CssClass="button" ID="btnPlay" runat="server" Text="PLAY" onclick="btnPlay_Click" Height="30px" Width="60px" />
+ 
+    <div style="float: right;height: 300px; width:auto">
+        <p >
+            <asp:Image ID="image1" runat="server" ImageUrl="~/images/0.jpg" Style="height: 200px; width:200px"/>
+          
+        </p>
+        </div>
+
+        <br />
+        <br />
+        <asp:Label ID="lblHint" runat="server" Text="Label5" CssClass="hint" ForeColor="#FF3300" Width="200px"></asp:Label>
+        <br />
+ 
+
+        
+    <asp:Label ID="lblWord" runat="server" Text="Label" CssClass="word"></asp:Label>
+     
+     
+    
+    <br />
+
+        
+    <asp:Label ID="lblMessage" runat="server" CssClass="attempts" Text="Lives left:" BorderStyle="Solid"></asp:Label>
+    <asp:Label ID="lblAttemps" runat="server" CssClass="word"></asp:Label>
+    <p>
+    <asp:Label ID="lblResult" runat="server" CssClass="word"></asp:Label>
+    
+   <br />
+    <div>
+        <asp:Label ID="lblNewWord" runat="server" ForeColor="#3333CC">Add New Word</asp:Label>&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br />
+        <asp:TextBox ID="txtName" runat="server" Width="92px" ></asp:TextBox>
+    &nbsp;&nbsp;&nbsp;&nbsp;<br />
+        <asp:Label ID="lblNewHint" runat="server" Text="Add New Hint" ForeColor="#3333CC"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtHint" runat="server" Width="156px" ></asp:TextBox>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnAdd" runat="server" Height="30px" Text="Add New Word" Width="116px" OnClick="btnAdd_Click" />
+
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnRemove" runat="server" Height="26px" OnClick="btnRemove_Click" Text="Remove Word" Width="109px" />
+
+        <br />
+        <br />
+        <br />
+
+    </div>
+   
+</form>
+    </body>
+</html>
+
---
+++ Hungman/Hungman_WebForm.aspx.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace Hungman_Game__HW2
+{
+    public partial class Hungman_WebForm : System.Web.UI.Page
+    {
+        string word;
+        string secret;
+        string maskedWord;
+        int attemptCount = 5;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* Submitting the ASP.NET page to the server for processing*/
+            if (!IsPostBack)
+            {
+                Hungman();
+
+            }
+            /*Perform the actions that are common to all requests, database query */
+            else
+            {
+                word = (string)ViewState["word"];
+                secret = (string)ViewState["secret"];
+                maskedWord = (string)ViewState["maskedWord"];
+                attemptCount = (int)ViewState["attemptCount"];
+            }
+
+        }
+        /* Perform any updates before the output is rendered. */
+        protected void Page_PreRender(Object sender, EventArgs e)
+        {
+            ViewState["word"] = word;
+            ViewState["secret"] = secret;
+            ViewState["maskedWord"] = maskedWord;
+            ViewState["attemptCount"] = attemptCount;
+        }
+
+        /*The class to run the game*/
+         protected void Hungman()
+         {
+             A.Enabled = true; B.Enabled = true; C.Enabled = true; D.Enabled = true; E.Enabled = true;
+             F.Enabled = true; G.Enabled = true; H.Enabled = true; I.Enabled = true; J.Enabled = true;
+             K.Enabled = true; L.Enabled = true; M.Enabled = true; N.Enabled = true; O.Enabled = true;
+             P.Enabled = true; Q.Enabled = true; R.Enabled = true; S.Enabled = true; T.Enabled = true;
+             U.Enabled = true; V.Enabled = true; W.Enabled = true; X.Enabled = true; Y.Enabled = true;
+             Z.Enabled = true; btnPlay.Enabled = false;
+             
+            attemptCount =6;
+            image1.ImageUrl = "~/images/0.jpg";
+            /* Creating Array List where to store the name and the hint attributes*/
+            ArrayList secret_words = new ArrayList();
+             ArrayList explained_words = new ArrayList();
+
+             Random random = new Random();
+            /*By using exeption handeling, load the Xml file and display in specific labels the attributes name and hint of the element phrase*/
+             try
+             {
+                 XmlDocument xDocument = new XmlDocument();
+                 xDocument.Load(Server.MapPath("DataSet.xml"));
+                 XmlNodeList xNode = xDocument.SelectNodes("Phrases/phrase");
+                 foreach (XmlNode dataline in xNode)
+                 {
+
+                     secret_words.Add(dataline.Attributes["name"].Value.ToString());
+                     explained_words.Add(dataline.Attributes["hint"].Value.ToString());
+
+                 }
+
+                 /*Storing into a string the secret and explained words*/
+                 string[] findss = secret_words.ToArray(typeof(string)) as string[];
+                 string[] hintss = explained_words.ToArray(typeof(string)) as string[];
+                 int _random = random.Next(findss.Length);
+
+                 secret = hintss[_random];
+                 lblHint.Text = secret;
+
+                 word = findss[_random];
+                 maskedWord = word;
+                /*Using regular expression to mask the word*/
+                 maskedWord = Regex.Replace(word, "[A-z]", "-");
+                 lblWord.Text = maskedWord;
+                 lblMessage.Text = "Lives left: ";
+                 lblAttemps.Text = attemptCount.ToString();
+                 lblResult.Text = "";
+
+             }
+             catch (Exception ex)
+             {
+                 lblWord.Text = "Cannot read the file " + ex;
+             }
+
+         }
+
+        protected void LetterGuessed(object sender, EventArgs e)
+        {
+
+            if (attemptCount != 0 && (lblResult.Text.Equals("")))
+            {
+                string letter = ((Control)sender).ID.ToString().ToLower();
+                Button btn = (Button)sender;
+                btn.Enabled = false;
+                int startPosition = word.IndexOf(letter);
+                /*When the letter is guessed right*/
+                if (!(startPosition == -1))
+                {
+                    do
+                    {
+                        maskedWord = maskedWord.Remove(startPosition, 1).Insert(startPosition, letter);
+                        lblWord.Text = maskedWord;
+                        startPosition = word.IndexOf(letter, startPosition + 1);
+                    }
+                    while (startPosition != -1);
+                }
+                /*If the letter is guessed wrong the parts of the pictures are drawn*/
+                else
+                {
+                    attemptCount--;
+                    if (attemptCount == 5)
+                    {
+                        image1.ImageUrl = "~/images/1.jpg";
+
+                    }
+                    if (attemptCount == 4)
+                    {
+                        image1.ImageUrl = "~/images/2.jpg";
+
+                    }
+                    if (attemptCount == 3)
+                    {
+                        image1.ImageUrl = "~/images/3.jpg";
+                    }
+                    if (attemptCount == 2)
+                    {
+                        image1.ImageUrl = "~/images/4.jpg";
+                    }
+                    if (attemptCount == 1)
+                    {
+                        image1.ImageUrl = "~/images/5.jpg";
+                    }
+                    if (attemptCount == 0)
+                    {
+                        image1.ImageUrl = "~/images/6.jpg";
+                    }
+                    lblAttemps.Text = attemptCount.ToString();
+                }
+                /*If the word is guessed correct*/
+                if (maskedWord.Equals(word))
+                {
+                    btnPlay.Enabled = true;
+                    lblWord.Text = word;
+                    lblResult.Text = "You Won";
+                    lblMessage.Text = "PLAY AGAIN";
+                    lblAttemps.Text = "";
+                }
+
+                /*If the word is not guessed right and if the number of lives is 0*/
+                if (attemptCount == 0 && !(maskedWord.Equals(word)))
+                {
+                    lblResult.Text ="TRY AGAIN!";
+                    lblWord.Text = word;
+                    btnPlay.Enabled = true;
+                    lblMessage.Text = "DEAD :(";
+                    lblAttemps.Text = "";
+                }
+
+            }
+            else
+            {
+                //Do nothing on letter button click
+            }
+        }
+       
+        /*Button when clicking the Play Button*/
+        protected void btnPlay_Click(object sender, EventArgs e)
+        {
+            Hungman();
+        }
+
+        /*Button when Adding a new word*/
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load(Server.MapPath("~/DataSet.xml"));
+
+            XmlElement ParentElement = doc.CreateElement("phrase");
+
+            XmlAttribute Name = doc.CreateAttribute("name");
+
+            Name.InnerText = txtName.Text;
+            doc.DocumentElement.SetAttributeNode(Name);
+
+            XmlAttribute Hint= doc.CreateAttribute("hint");
+
+            Hint.InnerText = txtHint.Text;
+
+            doc.DocumentElement.SetAttributeNode(Hint);
+
+            doc.Save(Server.MapPath("~/DataSet.xml"));
+
+
+
+        }
+
+        protected void btnRemove_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
